8feb76130fb977cb209a5511c50d9c27e751fd77
at-hoavo <hoa.vo@asiantech.vn>
2017-10-22 09:56:28 +0700

    merge master

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import {Cart} from "./src/app/component/first/Cart";
 
 AppRegistry.registerComponent('MomCook', () => Cart);
