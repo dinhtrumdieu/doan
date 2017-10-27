9acd71cbea5815d15bc24d4cd160a5082553b366
nguyentrungdinh <dinhnt@bap.jp>
2017-10-27 17:13:21 +0700

    add change infor

--- src/app/component/you/ChangeInforComponent.js
+++ src/app/component/you/ChangeInforComponent.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
 import {
-    View
+    View, StyleSheet, Image, TouchableOpacity, CheckBox
 } from 'react-native';
-export default class ItemDetail extends Component {
+import ToolBar from "../common/ToolBar";
+import Text from "../common/Text";
+export default class ChangeInforComponent extends Component {
     render() {
         return (
-            <View style={{flex:1}}>
-
+            <View style={{flex:1,backgroundColor:'#aaa'}}>
+                <ToolBar title='Chỉnh sửa thông tin'/>
+                <View style={{flexDirection:'row',backgroundColor:'#fff',paddingVertical:15}}>
+                    <Image source={require('../../../res/img/bg_app.jpg')} style={styles.Left}/>
+                    <View style={{flex:1}}>
+                        <Text>Nguyen Trung Dinh</Text>
+                        <View style={{width: '100%', borderWidth: 0.8, borderColor: '#c6c6c6', marginVertical: 10}}/>
+                        <View style={{flexDirection:'row',alignItems:'center'}}>
+                            <CheckBox/>
+                            <Text style={{marginRight:30}}>Nam</Text>
+                            <CheckBox/>
+                            <Text>Nữ</Text>
+                        </View>
+                        <View style={{width: '100%', borderWidth: 0.8, borderColor: '#c6c6c6', marginVertical: 10}}/>
+                        <Text>35 Nguyen Chanh</Text>
+                    </View>
+                </View>
+                <TouchableOpacity style={{
+                    width: 150,
+                    height: 35,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: 'green',
+                    borderRadius: 5,
+                    alignSelf: 'center',
+                    marginTop: 30
+                }}>
+                    <Text style={{color: 'white', fontSize: 14}}>Cập nhật</Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    Left:{
+        width:60,
+        height:60,
+        borderRadius:30,
+        marginHorizontal:20,
+    },
+    Right:{
+
+    }
+});
